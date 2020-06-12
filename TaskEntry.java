edde5b177453288476e575e7185137f61e82d2f2
Kaustubh Wadagavi <kaustubhwadagavi@gmail.com>
2020-06-12 18:49:02 +0530

    Implemeted getters, setters and methods to add new entry

---
+++ TaskEntry.java
@@ -0,0 +1,67 @@
+import java.io.Serializable;
+
+public class TaskEntry implements Serializable {
+
+    private String startTime;
+    private String endTime;
+    private String taskType;
+    private String taskName;
+    private String taskDescription;
+
+    public TaskEntry() {
+
+    }
+
+    public TaskEntry(String startTime, String endTime, String taskType, String taskName, String taskDescription) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.taskType = taskType;
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
+
+    public String getTaskType() {
+        return taskType;
+    }
+
+    public void setTaskType(String taskType) {
+        this.taskType = taskType;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getDescription() {
+        return taskDescription;
+    }
+
+    public void setDescription(String taskDescription) {
+        this.taskDescription = taskDescription;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuffer("Start Time :").append(this.startTime).append("\nEnd Time :").append(this.endTime).append("\nType :").append(this.taskType).append("\nTask Name :").append(this.taskName).append("\nDescription :").append(this.taskDescription).toString();
+    }
+}
